ba321c561671db8dc1f69f0e11f77f0b549edd7e
Beetroot16 <vishrut.deshmukh@somaiya.edu>
2022-05-19 16:13:49 +0530

    Exp 5

---
+++ Exp5.py
@@ -0,0 +1,31 @@
+# def palin(s,start,end):
+#     if start >= end:
+#         return True
+#     if s[start] == s[end]:
+#         return palin(s,start+1,end-1)
+#     else:
+#         return False
+
+
+# s = list(input("Enter the string: "))
+# print(palin(s,0,len(s)-1)) # 1 2 3 2 1 
+
+# num = list(map(int,input("Enter the numbers: ").split()))
+
+# even = filter(lambda x: x%2==0,num)
+# odd =  filter(lambda x: x%2!=0,num)
+
+# print(list(even))
+# print(list(odd))
+
+# def fact(n):
+#     if n == 1:
+#         return n
+#     else:
+#         return n*fact(n-1)
+
+# n = int(input("Enter the number: "))
+# print(fact(n))
+
+
+
---
+++ test2.py
@@ -0,0 +1,10 @@
+t = int(input())
+
+for x in range(t):
+    r = int(input())
+    if r<=50:
+        print("100")
+    elif 50<r<100:
+        print("50")
+    else:
+        print("0")
